b8518141c2988dfecb3ed59e3de6062a25246564
Aloneboy2024 <161654219+Aloneboy2024@users.noreply.github.com>
2024-04-02 11:28:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ VRFY_IMG = environ.get("VRFY_IMG", "https://telegra.ph/file/86d3038e112e629142ed
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002024771917').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '0').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
