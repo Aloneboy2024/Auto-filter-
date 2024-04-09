2fe4157b6570e4ad7c7ebae21a249c508478066f
Aloneboy2024 <161654219+Aloneboy2024@users.noreply.github.com>
2024-04-09 11:42:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<Abdul2024>:<abdul54321>@cluster0.6xep2fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<Abdul2024>:<abdul54321>@cluster0.6xep2fb.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "THHT2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
